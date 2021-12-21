4cf047c3ad12df5728df1b84adacac8dd05a9383
Jacob_Peng <Jacob_Peng@epam.com>
2021-12-21 13:34:20 +0800

    add dp

---
+++ Algorthms/Algorthms/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorthms
+{
+    internal class BinaryTreeInorderTraversal
+    {
+        //跌代法，使用栈
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            
+            IList<int> results = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while (root != null || stack.Count() > 0)
+            {
+
+                while (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+
+                }
+                root = stack.Pop();
+                results.Add(root.val);
+                root = root.right;
+            }
+            return results;
+        }
+        //
+        //IList<int> results = new List<int>();
+        //public IList<int> InorderTraversal(TreeNode root)
+        //{
+        //    InOrder(root);
+        //    return results;
+        //}
+        //public void InOrder(TreeNode root)
+        //{
+
+        //    if (root == null) return;
+        //    InorderTraversal(root.left);
+        //    results.Add(root.val);
+        //    InorderTraversal(root.right);
+        //}
+
+    }
+
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+                 }
+  }
+
+}
---
+++ Algorthms/Algorthms/ClimbStair.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorthms
+{
+    internal class ClimbStair
+    {
+        //递归代码
+        public int climbStairs(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            return climbStairs(n - 1) + climbStairs(n - 2);
+        }
+        //记忆化递归
+        Dictionary<int,int> memo =new Dictionary<int, int>() ;
+        public int climbStairs1(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            if (memo.ContainsKey(n)) return memo[n];
+            memo.Add(n, climbStairs(n - 1) + climbStairs(n - 2));
+            return memo[n];
+        }
+        //DP
+        public int climbStairsDP(int n)
+        {
+            if (n == 1) return 1;
+            int[] dp = new int[10];
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[dp.Length - 1];
+        }
+        //DP滚动数组优化
+        public int climbStairsDP2(int n)
+        {
+            if (n == 1) return 1;
+            int a = 1;
+            int b= 2;
+            int temp = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return temp;
+        }
+
+    }
+}
